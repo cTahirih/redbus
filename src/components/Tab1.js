b05dac9d4414d5147a829169a9545f2246ef347f
tayrih <tahysluana@gmail.com>
2018-03-20 01:03:51 -0500

    agrega menu tab al componente main

---
+++ src/components/Tab1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Row from './Row';
+
+const Tab1 = () => (
+  <div className="container">
+    <Row>
+      <p>llhjhbj</p>
+    </Row>
+  </div>
+)
+
+export default Tab1;
