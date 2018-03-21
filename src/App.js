c3a55536ba7b42b8e664dd83cbcf105bfe781f75
tayrih <tahysluana@gmail.com>
2018-03-21 18:46:33 -0500

    add a new slider tab2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Main from './components/Main';
-import ControlledCarousel from './components/carousel'
 
 const App = () => (
   <div className="row">
     <Main />
-    <ControlledCarousel />
   </div>
 )
 
