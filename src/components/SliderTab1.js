92379f714407e96f761a54c0f3e0d048afe4fc99
LizbethFP <lizbeth.fp@gmail.com>
2018-03-21 22:41:49 -0500

    corrigiendo slider carousel tab 1

--- src/components/SliderTab1.js
+++ src/components/SliderTab1.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
-import SliderArrowTab1 from './SliderArrowTab1';
+// import SliderArrowTab1 from './SliderArrowTab1';
 import SliderBankIcons from './SliderBankIcons'
 
 const SliderTab1 = ({ dataSliderInternet }) => (
   <Row className="show-grid">
     <div className="col-12">
       <Row className="show-grid">
-        <SliderArrowTab1 />
+        {/*<SliderArrowTab1 />*/}
         <SliderBankIcons dataIconBanks={dataSliderInternet }/>       
       </Row>
     </div>
