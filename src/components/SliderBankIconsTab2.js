7e4ea5bad6a792adaad0ef7c108145379c6bc279
tayrih <tahysluana@gmail.com>
2018-03-21 22:50:24 -0500

    añade dependencias redux

--- src/components/SliderBankIconsTab2.js
+++ src/components/SliderBankIconsTab2.js
@@ -9,7 +9,7 @@ const SliderBankIconsTab2 = ({ dataIconCash }) => {
     infinite: false,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 3
   };
   return (
     <div className="container">
